3017b266358c595475a7cda1d32a105a90420e24
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-09-26 09:02:27 +0200

    #develop: create DTO class for news

---
+++ app/src/main/java/it/paolomazza/newsapp/data/dto/GetNewsListResponse.kt
@@ -0,0 +1,25 @@
+package it.paolomazza.newsapp.network
+
+import com.google.gson.annotations.SerializedName
+
+data class GetNewsListDTO(
+        @SerializedName("message")
+        val message: NewsListDTO?
+) {
+
+    data class NewsListDTO(
+            @SerializedName("results")
+            val result: List<NewsDTO>?
+    )
+
+    data class NewsDTO(
+            @SerializedName("id")
+            val id: Int,
+            @SerializedName("title")
+            val title: String?,
+            @SerializedName("subtitle")
+            val subtitle: String?,
+            @SerializedName("timestamp")
+            val timestamp: Long?
+    )
+}
