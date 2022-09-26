593638cc0ec391d3373acfb3910cc321bd519d97
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-09-26 09:04:07 +0200

    #develop: create API class for invoking API service

---
+++ app/src/main/java/it/paolomazza/newsapp/data/API.kt
@@ -0,0 +1,18 @@
+package it.paolomazza.newsapp.data
+
+import it.paolomazza.newsapp.network.GetNewsListDTO
+import it.paolomazza.newsapp.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface API {
+
+    @GET("news")
+    @Headers("x-api-key: ${Constants.API_KEY}")
+    suspend fun getNews(
+            @Query("offset") offset: Int,
+            @Query("limit") limit: Int = Constants.DEFAULT_NEWS_LIMIT
+    ): GetNewsListDTO
+
+}
