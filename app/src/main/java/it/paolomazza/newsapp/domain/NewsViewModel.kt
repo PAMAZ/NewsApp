bcb15ee536d6408349fedbb370d6f171a42876f7
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-09-26 11:29:05 +0200

    #develop: create NewsViewModel class

---
+++ app/src/main/java/it/paolomazza/newsapp/domain/NewsViewModel.kt
@@ -0,0 +1,38 @@
+package it.paolomazza.newsapp.domain
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import dagger.hilt.android.lifecycle.HiltViewModel
+import it.paolomazza.newsapp.presentation.adapter.item.BaseItem
+import it.paolomazza.newsapp.presentation.adapter.item.NewsItem
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel @Inject constructor(
+        private val newsUseCase: NewsUseCase):ViewModel() {
+
+    private val _privateNewsLiveData = MutableLiveData<PagingData<out BaseItem>>()
+    val newsLiveData: LiveData<PagingData<out BaseItem>> = _privateNewsLiveData
+
+    @Suppress("UNCHECKED")
+    fun getNewsList() {
+        viewModelScope.launch {
+             newsUseCase.getNewsApi().map { pagingdata ->
+                pagingdata.map {
+                    NewsItem(it)
+                }
+            }.cachedIn(viewModelScope).collectLatest {
+                _privateNewsLiveData.value = it
+             }
+        }
+    }
+
+}
