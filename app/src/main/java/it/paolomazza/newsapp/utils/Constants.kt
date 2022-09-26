3a9dd42910138e0ceb4059321ba902b11f6f3779
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-09-26 11:27:49 +0200

    #develop: create Constants file

---
+++ app/src/main/java/it/paolomazza/newsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package it.paolomazza.newsapp.utils
+
+object Constants {
+
+    const val BASE_URL = "https://aewvdpjnii.execute-api.eu-central-1.amazonaws.com/dev/"
+    const val API_KEY = "B1Ryz3vdoW5E2VvbLSCKo7MhWoGXJmgY2TrPikRe"
+
+    const val DEFAULT_NEWS_LIMIT = 20
+
+}
