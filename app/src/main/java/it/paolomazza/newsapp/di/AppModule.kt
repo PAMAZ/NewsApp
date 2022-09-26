9fb6291917b41cbbbcbf9a7bef12a7f45dda41f5
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-09-26 08:59:00 +0200

    #develop: create AppModule class

---
+++ app/src/main/java/it/paolomazza/newsapp/di/AppModule.kt
@@ -0,0 +1,30 @@
+package it.paolomazza.newsapp.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import it.paolomazza.newsapp.App
+import javax.inject.Singleton
+
+@Module
+@Suppress("unused")
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): App {
+        return app as App
+    }
+
+    @Provides
+    @Singleton
+    fun provideContext(application: App): Context {
+        return application.applicationContext
+    }
+
+
+}
