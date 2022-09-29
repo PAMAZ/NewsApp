c3cac86ed7746651df627fb1bcf9e17125c0027d
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-09-29 11:03:53 +0200

    #develop: Add JUNIT tests for detail class

--- app/src/main/java/it/paolomazza/newsapp/presentation/fragment/NewsListFragment.kt
+++ app/src/main/java/it/paolomazza/newsapp/presentation/fragment/NewsListFragment.kt
@@ -1,6 +1,5 @@
 package it.paolomazza.newsapp.presentation.fragment
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.paging.CombinedLoadStates
--- app/src/test/java/it/paolomazza/newsapp/RetrofitServiceTest.kt
+++ app/src/test/java/it/paolomazza/newsapp/RetrofitServiceTest.kt
@@ -47,12 +47,28 @@ class RetrofitServiceTest {
     fun getNewsList() {
         runBlocking {
             val mockResponse = MockResponse()
-            val file = getFile("news.json")
+            val file = getFile("news_list.json")
             mockWebServer.enqueue(mockResponse.setBody(file))
             val response = apiService.getNews(1,1)
             val request = mockWebServer.takeRequest()
             assertEquals("/news?offset=1&limit=1",request.path)
             assertEquals(true, response.message?.result?.size == 20)
+        }
+    }
+
+    @Test
+    fun getNewsDetail() {
+        runBlocking {
+            val mockResponse = MockResponse()
+            val file = getFile("news_detail.json")
+            mockWebServer.enqueue(mockResponse.setBody(file))
+            val response = apiService.getNewsDetail(0)
+            val request = mockWebServer.takeRequest()
+            assertEquals("/news/0",request.path)
+            assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit",response.message.title)
+            assertEquals(" Curabitur finibus est et nibh suscipit, ac venenatis odio elementum",response.message.subtitle)
+            assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus est et nibh suscipit, ac venenatis odio elementum. Pellentesque molestie, nisi eget blandit malesuada, neque diam vestibulum mi, eget pharetra nunc dolor id odio. Quisque eleifend lacus ante, et pellentesque tellus hendrerit sit amet. Curabitur in libero quam. Nulla lorem massa, malesuada eleifend blandit ac, lacinia dignissim turpis. In lacus nisi, suscipit nec tristique ut, euismod malesuada quam. Morbi et risus ante. Proin luctus vel leo consectetur sagittis. Donec aliquet, ipsum in tincidunt accumsan, purus magna bibendum felis, varius euismod leo nulla sit amet arcu. Etiam feugiat elit lectus, eu euismod est semper id. Morbi nibh tortor, auctor dignissim nisl ut, tincidunt eleifend purus. Fusce in ante diam. Proin odio est, consequat sit amet arcu et, consequat pharetra libero. Morbi ac elit felis.",response.message.body)
+            assertEquals(1624443499409, response.message.timestamp)
         }
     }
 
