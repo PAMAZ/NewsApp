fce9eaba1f4b6c4fef571bba1ea25b3ffad09cca
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-09-29 10:54:47 +0200

    #develop: add toolbar in activity

--- app/src/main/java/it/paolomazza/newsapp/MainActivity.kt
+++ app/src/main/java/it/paolomazza/newsapp/MainActivity.kt
@@ -1,13 +1,28 @@
 package it.paolomazza.newsapp
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true);
+            setDisplayShowHomeEnabled(true);
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId === android.R.id.home) {
+            super.onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
