beeb6d0afee6f772f1d9dc9d51eb6591a31f523e
Paolo Mazza <paolo.mazza@consulenti.mediolanum.it>
2022-10-15 10:42:57 +0200

    #develop: remove API KEY

--- app/src/main/java/it/paolomazza/newsapp/utils/Constants.kt
+++ app/src/main/java/it/paolomazza/newsapp/utils/Constants.kt
@@ -3,7 +3,7 @@ package it.paolomazza.newsapp.utils
 object Constants {
 
     const val BASE_URL = "https://aewvdpjnii.execute-api.eu-central-1.amazonaws.com/dev/"
-    const val API_KEY = "B1Ryz3vdoW5E2VvbLSCKo7MhWoGXJmgY2TrPikRe"
+    const val API_KEY = "********************"
 
     const val DEFAULT_NEWS_LIMIT = 20
 
